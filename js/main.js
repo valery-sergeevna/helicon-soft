f2c0ce988880a895f4eea868c7790530105e7de2
Valery <lekalera777@gmail.com>
2020-09-17 23:18:45 +0300

    initial commit(creating header, nav)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    autoplay:true,
+    speed: 600,
+    effect: 'slide',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+})
